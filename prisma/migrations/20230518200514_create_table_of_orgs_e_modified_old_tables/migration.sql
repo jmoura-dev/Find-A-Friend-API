d6f8bcb5f21fa1927a723aa5569b20c64e866476
Jackson <jackson1712@hotmail.com>
2023-05-18 19:19:18 -0300

    added new migrations to modified database

---
+++ prisma/migrations/20230518200514_create_table_of_orgs_e_modified_old_tables/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `details` on the `pets` table. All the data in the column will be lost.
+  - You are about to drop the column `race` on the `pets` table. All the data in the column will be lost.
+  - You are about to drop the column `address` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `whatsapp` on the `users` table. All the data in the column will be lost.
+  - Added the required column `age` to the `pets` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `breed` to the `pets` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `pets` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `size` to the `pets` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password_hash` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "pets" DROP COLUMN "details",
+DROP COLUMN "race",
+ADD COLUMN     "about" TEXT,
+ADD COLUMN     "age" TEXT NOT NULL,
+ADD COLUMN     "avatar_pet" TEXT,
+ADD COLUMN     "breed" TEXT NOT NULL,
+ADD COLUMN     "name" TEXT NOT NULL,
+ADD COLUMN     "size" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "address",
+DROP COLUMN "whatsapp",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password_hash" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "whatsapp_number" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'MEMBER',
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");

e51d9e2b790ddd1a2a93e7ea98d22fbd3b12bef6
Jackson <jackson1712@hotmail.com>
2023-05-23 13:47:30 -0300

    refactor

--- src/services/users/create.ts
+++ src/services/users/create.ts
@@ -1,17 +1,26 @@
 import { UsersRepository } from '@/repositories/users.repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from '../errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
-interface Data {
+interface CreateServiceRequest {
   name: string
   email: string
   password: string
 }
 
+interface CreateServiceResponse {
+  user: User
+}
+
 export class CreateService {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: Data) {
+  async execute({
+    name,
+    email,
+    password,
+  }: CreateServiceRequest): Promise<CreateServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -20,10 +29,14 @@ export class CreateService {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }

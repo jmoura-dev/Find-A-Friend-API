70544f4620a20cf92e1d0b3f8c7884315af22e79
Jackson <jackson1712@hotmail.com>
2023-05-24 20:02:34 -0300

    refactor

--- src/repositories/in-memory/in-memory-repository.ts
+++ src/repositories/in-memory/in-memory-repository.ts
@@ -1,5 +1,6 @@
 import { User, Prisma, Role } from '@prisma/client'
 import { UsersRepository } from '../users.repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryRepository implements UsersRepository {
   public items: User[] = []
@@ -13,12 +14,12 @@ export class InMemoryRepository implements UsersRepository {
     return user
   }
 
-  async create(data: Prisma.UserCreateInput) {
+  async create({ name, email, password_hash }: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-01',
-      name: data.name,
-      email: data.email,
-      password_hash: data.password_hash,
+      id: randomUUID(),
+      name,
+      email,
+      password_hash,
       role: Role.MEMBER,
       created_at: new Date(),
       updated_at: new Date(),

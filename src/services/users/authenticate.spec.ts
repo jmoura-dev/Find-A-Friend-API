325652432c6d67fe9ec94486964411f3ce0e4e36
Jackson <jackson1712@hotmail.com>
2023-05-24 09:36:37 -0300

    tests: Adding missing tests of authenticate

---
+++ src/services/users/authenticate.spec.ts
@@ -0,0 +1,67 @@
+import { InMemoryRepository } from '@/repositories/in-memory/in-memory-repository'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { AuthenticateService } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+
+let usersRepository: InMemoryRepository
+let sut: AuthenticateService
+
+describe('Authenticate service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryRepository()
+    sut = new AuthenticateService(usersRepository)
+  })
+
+  it('Must be able to authenticate', async () => {
+    const password_hash = await hash('123456', 6)
+
+    await usersRepository.create({
+      name: 'fulano',
+      email: 'fulano@email.com',
+      password_hash,
+    })
+
+    const { user } = await sut.execute({
+      email: 'fulano@email.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('Must not be able to authenticate with invalid email', async () => {
+    const password_hash = await hash('123456', 6)
+
+    await usersRepository.create({
+      name: 'fulano',
+      email: 'fulano@email.com',
+      password_hash,
+    })
+
+    expect(async () => {
+      await sut.execute({
+        email: 'error@email.com',
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('Must not be able to authenticate with invalid password', async () => {
+    const password_hash = await hash('123456', 6)
+    const email = 'fulano@email.com'
+
+    await usersRepository.create({
+      name: 'fulano',
+      email,
+      password_hash,
+    })
+
+    expect(async () => {
+      await sut.execute({
+        email,
+        password: '1234256',
+      })
+    }).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})

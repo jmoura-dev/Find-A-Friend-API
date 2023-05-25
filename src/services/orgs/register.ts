73f62df65e5eaa02fe6a13452427bce1fcd862d8
Jackson <jackson1712@hotmail.com>
2023-05-25 15:58:04 -0300

    feat: create new service of orgs to handling with OrgsServices and tests

---
+++ src/services/orgs/register.ts
@@ -0,0 +1,51 @@
+import { OrgsRepository } from '@/repositories/orgs.repository'
+import { hash } from 'bcryptjs'
+import { EmailAlreadyRegistered } from '../errors/email-already-registered'
+import { Org } from '@prisma/client'
+
+interface RegisterOrgRequest {
+  id: string
+  name: string
+  email: string
+  password: string
+  address: string
+  whatsapp_number: string
+}
+
+interface RegisterOrgResponse {
+  org: Org
+}
+
+export class RegisterOrgService {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    id,
+    name,
+    email,
+    password,
+    address,
+    whatsapp_number,
+  }: RegisterOrgRequest): Promise<RegisterOrgResponse> {
+    const password_hash = await hash(password, 6)
+
+    const doesEmailSameExists = await this.orgsRepository.findByEmail(email)
+
+    if (doesEmailSameExists) {
+      throw new EmailAlreadyRegistered()
+    }
+
+    const org = await this.orgsRepository.create({
+      id,
+      name,
+      email,
+      password_hash,
+      address,
+      whatsapp_number,
+    })
+
+    return {
+      org,
+    }
+  }
+}

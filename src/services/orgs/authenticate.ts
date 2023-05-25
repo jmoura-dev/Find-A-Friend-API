6af7cfe27f9ade7985acbbfdc3558b5a8b8f1383
Jackson <jackson1712@hotmail.com>
2023-05-25 17:51:14 -0300

    feat: create authenticate service of orgs to done login

---
+++ src/services/orgs/authenticate.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs'
+import { Org } from '@prisma/client'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { OrgsRepository } from '@/repositories/orgs.repository'
+
+interface AuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateServiceResponse {
+  org: Org
+}
+
+export class AuthenticateService {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      org,
+    }
+  }
+}

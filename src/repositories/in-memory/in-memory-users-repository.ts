23bdc778c237562fa572bb5fc1997f321b8f34f8
Jackson <jackson1712@hotmail.com>
2023-05-25 16:01:19 -0300

    fix: alter name the function

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,32 @@
+import { User, Prisma, Role } from '@prisma/client'
+import { UsersRepository } from '../users.repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async create({ name, email, password_hash }: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+      password_hash,
+      role: Role.MEMBER,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}

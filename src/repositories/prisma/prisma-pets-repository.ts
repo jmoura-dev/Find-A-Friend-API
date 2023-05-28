e22694f37d70383ed131083009589bd286287e81
Jackson <jackson1712@hotmail.com>
2023-05-28 12:42:50 -0300

    feat: create prisma pets repository to done connection with database

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,45 @@
+import { Prisma } from '@prisma/client'
+import { PetsRepository } from '../pets.repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismPetsRepository implements PetsRepository {
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({
+      data,
+    })
+
+    return pet
+  }
+
+  async findById(id: string) {
+    const pet = await prisma.pet.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!pet) {
+      return null
+    }
+
+    return pet
+  }
+
+  async SearchManyByCharacteristics(
+    city: string,
+    age?: string | undefined,
+    breed?: string | undefined,
+    size?: string | undefined,
+  ) {
+    const pets = await prisma.pet.findMany({
+      where: {
+        city,
+        age,
+        breed,
+        size,
+      },
+    })
+
+    return pets
+  }
+}

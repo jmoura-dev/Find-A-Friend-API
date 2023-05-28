2ab21fa6479792736c203a59f3d72e39c45a3330
Jackson <jackson1712@hotmail.com>
2023-05-28 11:28:58 -0300

    feat: create fetch list pets by characteristics service with filter by city and characteristics, if there is

---
+++ src/services/pets/fetch-list-pets-by-characteristics.ts
@@ -0,0 +1,35 @@
+import { PetsRepository } from '@/repositories/pets.repository'
+import { Pet } from '@prisma/client'
+
+interface FetchListPetOnCityRequest {
+  city: string
+  age?: string
+  breed?: string
+  size?: string
+}
+
+interface FetchListPetOnCityResponse {
+  pets: Pet[]
+}
+
+export class FetchListPetOnCity {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute({
+    city,
+    age,
+    breed,
+    size,
+  }: FetchListPetOnCityRequest): Promise<FetchListPetOnCityResponse> {
+    const pets = await this.petsRepository.findManyByCharacteristics(
+      city,
+      age,
+      breed,
+      size,
+    )
+
+    return {
+      pets,
+    }
+  }
+}

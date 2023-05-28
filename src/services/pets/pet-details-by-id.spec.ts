0c26c868463b9c71c9779eb8e2bde0509f813a69
Jackson <jackson1712@hotmail.com>
2023-05-28 12:00:03 -0300

    test: create test to get pet by id and view details

---
+++ src/services/pets/pet-details-by-id.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { PetDetailsService } from './pet-details-by-id'
+
+let petsRepository: InMemoryPetsRepository
+let sut: PetDetailsService
+
+describe('Get pet details Service', () => {
+  beforeEach(() => {
+    petsRepository = new InMemoryPetsRepository()
+    sut = new PetDetailsService(petsRepository)
+  })
+
+  it('should be able to get pet by id', async () => {
+    const petRegister = await petsRepository.create({
+      name: 'Ralf',
+      age: '3',
+      breed: 'mutt',
+      city: 'maceió',
+      size: 'medium',
+      org_id: 'Your happy pet',
+    })
+
+    const { pet } = await sut.execute({ id: petRegister.id })
+
+    console.log(pet)
+
+    expect(pet.id).toEqual(expect.any(String))
+  })
+})

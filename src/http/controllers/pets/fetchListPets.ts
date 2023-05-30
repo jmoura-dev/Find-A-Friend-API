465e38c392226ce190edbe0f9d3d440068863114
Jackson <jackson1712@hotmail.com>
2023-05-30 13:37:35 -0300

    feat: added new controller to fetch list pets by characteristics as params

---
+++ src/http/controllers/pets/fetchListPets.ts
@@ -0,0 +1,42 @@
+import { makeFetchListPetOnCity } from '@/services/factories/make-fetch-list-pets-service'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function fetchListPets(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const characteristicsQuerySchema = z.object({
+    city: z.string(),
+    age: z.string(),
+    breed: z.string(),
+    size: z.string(),
+  })
+
+  const { city, age, breed, size } = characteristicsQuerySchema.parse(
+    request.query,
+  )
+
+  try {
+    const service = makeFetchListPetOnCity()
+
+    console.log(city, age, breed, size)
+
+    const pets = await service.execute({
+      city,
+      age,
+      breed,
+      size,
+    })
+
+    return reply.status(200).send({
+      pets,
+    })
+  } catch (err) {
+    if (err instanceof Error) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+}

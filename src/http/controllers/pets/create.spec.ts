55a565b0e8daa2146d0e494b812a0255c4c3def5
Jackson <jackson1712@hotmail.com>
2023-05-30 17:33:42 -0300

    test: added new test to create petss

---
+++ src/http/controllers/pets/create.spec.ts
@@ -0,0 +1,31 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateOrg } from '@/utils/test/create-and-authenticate-org'
+
+describe('ok', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const { token } = await createAndAuthenticateOrg(app)
+
+    const response = await request(app.server)
+      .post('/pets')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Lessie',
+        age: '7',
+        size: 'big',
+        breed: 'mutt',
+        city: 'juazeiro',
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
